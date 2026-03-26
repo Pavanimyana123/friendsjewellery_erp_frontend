69b6e3a76550207b9485f37e4f8a67afa934e2ed
pavanimyana123 <pavanimyana2000@gmail.com>
2026-03-26 14:32:20 +0530

    Pavani

--- src/Url/NodeBaseURL4.js
+++ src/Url/NodeBaseURL4.js
@@ -1,4 +1,7 @@
 const baseURL4 = "https://friendserp.newfriendsjewellers.com:8085";
 
+// const baseURL4 ="https://friendserp2.newfriendsjewellers.com:9095";
+
+
 
 export default baseURL4;
--- src/Url/NodeBaseURL5.js
+++ src/Url/NodeBaseURL5.js
@@ -1,4 +1,7 @@
 const baseURL5 = "https://friendsgoldsmith.newfriendsjewellers.com:9096";
 
 
+// const baseURL5 ="https://friendserp2.newfriendsjewellers.com:9095";
+
+
 export default baseURL5;
