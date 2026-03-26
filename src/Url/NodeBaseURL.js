f3fcc960899ae569e6b287d98c24a3274dfd3715
pavanimyana123 <pavanimyana2000@gmail.com>
2026-03-26 11:20:00 +0530

    first commit

---
+++ src/Url/NodeBaseURL.js
@@ -0,0 +1,12 @@
+// const baseURL = "http://localhost:5001"; 
+
+// const baseURL = "https://friendserp.newfriendsjewellers.com:8085";
+
+
+const baseURL ="https://friendserp2.newfriendsjewellers.com:9095";
+
+
+// const baseURL ="https://friendsgoldsmith.newfriendsjewellers.com:9096";
+
+
+export default baseURL;
